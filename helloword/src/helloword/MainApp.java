b7911cb1a600dd8a1eaa1549d9fb8d174bd40bd0
coding-shrimp <412393570@qq.com>
2017-05-10 11:32:48 +0800

    演示git提交项目

---
+++ helloword/src/helloword/MainApp.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package helloword;
+
+/***************************************************
+ * 文件名：MainApp.java
+ * 描  述： 演示git的使用
+ * 创建人：老九学堂·coding-shirmp
+ * 日  期：2017年5月10日
+ * 版  本：1.0
+ * 官  网：http://www.xuetang9.com
+ * 官方QQ：614940318（老九君）
+ *
+ **************************************************/
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
